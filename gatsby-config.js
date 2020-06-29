f892e29f84a3c32235d2b8e983c67c5c46ed3eb6
holly-searchengineunity <holly@searchengineunity.com>
2020-06-29 14:04:40 -0400

    301 redirect complete end to end

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const {
   NODE_ENV,
-  URL: NETLIFY_SITE_URL = 'http://www.techquanimous.com/',
+  URL: NETLIFY_SITE_URL = 'https://www.techquanimous.com/',
   DEPLOY_PRIME_URL: NETLIFY_DEPLOY_URL = NETLIFY_SITE_URL,
   CONTEXT: NETLIFY_ENV = NODE_ENV,
 } = process.env;
@@ -20,6 +20,19 @@ module.exports = {
     siteUrl,
   },
   plugins: [
+    `gatsby-plugin-netlify`,
+    // {
+    //   resolve: `gatsby-plugin-netlify`,
+    //   options: {
+    //     headers: {}, // option to add more headers. `Link` headers are transformed by the below criteria
+    //     allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
+    //     mergeSecurityHeaders: true, // boolean to turn off the default security headers
+    //     mergeLinkHeaders: true, // boolean to turn off the default gatsby js headers
+    //     mergeCachingHeaders: true, // boolean to turn off the default caching headers
+    //     transformHeaders: (headers, path) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
+    //     generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules for client only paths
+    //   },
+    // },
     `gatsby-plugin-react-helmet`,
     {
       resolve: 'gatsby-source-sanity',
